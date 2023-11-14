471ea651d3a1de1a8afef85693b8d4c11d018384
Prad153 <pradeepvaibav@gmail.com>
2023-11-14 18:41:18 +0530

    commit 1

--- newtry/src/main/java/gitPush/newtry/learning_test.java
+++ newtry/src/main/java/gitPush/newtry/learning_test.java
@@ -1,11 +1,26 @@
 package gitPush.newtry;
 
-public class learning {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
+public class learning_test {
+
+//	public static void main(String[] args) {
+	
+	@Test
+	public void calci() {
 		int a=5, b = 10 ;
 		System.out.println("sum of a+b: " +(a+b) );
 		System.out.println("1 main end");
+		
+		
 	}
 	
+	
+	@Test
+	public void assertMethod() {
+		System.out.println("Inside assertMethod - 7+2: "+ (7+2) );
+		Assert.assertFalse(true);
+		
+	}
 }
