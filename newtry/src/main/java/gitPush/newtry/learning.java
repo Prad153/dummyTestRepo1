db3f570c3451a02231654820a42d4e26d1e380bd
Prad153 <pradeepvaibav@gmail.com>
2023-11-14 10:40:31 +0530

    pushing newtry project to Git 1

---
+++ newtry/src/main/java/gitPush/newtry/learning.java
@@ -0,0 +1,11 @@
+package gitPush.newtry;
+
+public class learning {
+
+	public static void main(String[] args) {
+		int a=5, b = 10 ;
+		System.out.println("sum of a+b: " +(a+b) );
+		System.out.println("1 main end");
+	}
+	
+}
